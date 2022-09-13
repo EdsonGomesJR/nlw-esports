a49e8313047d69f4209f8c4c158e0126e2651cc5
EdsonGomesJR <edyz86@hotmail.com>
2022-09-13 16:08:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-wgxbyx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-wgxbyx)
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className="w-8 h-8">
+      <h1> Hello World </h1>
+    </div>
+  );
+}
+
+export default App;
